cae91ed38578ef607e9421a4789a211947289320
Maria Lomeli <maria.lomeli@gupshup.io>
2026-02-09 22:20:36 -0400

    adding webhook logic

--- src/handlers/sessionHandler.js
+++ src/handlers/sessionHandler.js
@@ -7,7 +7,6 @@ const readFile = async () => {
     try {
         const file = await fs.readFile(SESSION_FILE, "utf8");
         const jsonData = JSON.parse(file);
-        const sessions = jsonData.sessions;
         return jsonData;
     }
     catch (error) {
@@ -18,24 +17,16 @@ const readFile = async () => {
 
 
 const editFile = async (phone, name, el) => {
-    const file = await fs.readFile(SESSION_FILE, "utf8");
-    const jsonData = JSON.parse(file);
-    console.log('Current JSON data:', jsonData);
-    const sessions = jsonData.sessions;
-    if (sessions[phone] == undefined) {
-        sessions[phone] = {};
-    }
-
-    console.log(sessions[phone])
-    Object.assign(sessions[phone], { [name]: el });
-    console.log(sessions[phone])
-
-    // // Update the JSON data
-    jsonData.sessions = sessions;
-    console.log(sessions)
-    // // Write the updated JSON data back to the file
     try {
-        const file = await fs.writeFile(filePath, JSON.stringify(jsonData, null, 2));
+        const file = await fs.readFile(SESSION_FILE, "utf8");
+        const jsonData = JSON.parse(file);
+        const sessions = jsonData.sessions;
+        if (sessions[phone] == undefined) {
+            sessions[phone] = {};
+        }
+        Object.assign(sessions[phone], { [name]: el });
+        jsonData.sessions = sessions;
+        await fs.writeFile(SESSION_FILE, JSON.stringify(jsonData, null, 2));
         console.log("JSON file updated successfully!");
     }
     catch (err) {
@@ -46,19 +37,13 @@ const editFile = async (phone, name, el) => {
 }
 
 const emptySession = async (phone) => {
-    const jsonData = await fs.readFile(SESSION_FILE, "utf8");
-    const sessions = jsonData.sessions;
-    console.log('entro', sessions[phone])
-
-    sessions[phone] = {};
-    console.log(sessions[phone])
-    // // Update the JSON data
-    jsonData.sessions = sessions;
-    console.log(sessions)
-    // // Write the updated JSON data back to the file
     try {
-        const file = await fs.writeFile(filePath, JSON.stringify(jsonData, null, 2));
-        console.log("JSON file updated successfully!");
+        const jsonData = await fs.readFile(SESSION_FILE, "utf8");
+        const sessions = jsonData.sessions;
+        sessions[phone] = {};
+        jsonData.sessions = sessions;
+        await fs.writeFile(SESSION_FILE, JSON.stringify(jsonData, null, 2));
+        console.log("JSON file emptied successfully!");
     }
     catch (err) {
         console.error(err);
